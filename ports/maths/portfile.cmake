f02bdb66b381f4065a0727a1e65901fad216a9c2
jeff <jeff665547@gmail.com>
2022-05-26 10:52:51 +0800

    Disable the automatical install.

--- ports/maths/portfile.cmake
+++ ports/maths/portfile.cmake
@@ -3,11 +3,9 @@ vcpkg_from_gitlab(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO centrillion/remoterepo
     REF v0.1.0-vcpkg  # Specify the version tag or the commit SHA from the remote repository.
-    SHA512  f05fe51888b21c4f30ee898e4ff4cd32a913ca8660487b3e4d1679ff60fdc4db2449d02a4797db5c7682e0de9c520a7fb307e90091c9ab274d33988624947cef
+    SHA512  76c6d186f06babefd45878a149c00ffc9754ef2f5173e2dd2a5b5244f852c2aefdf2c10a76b2d0515f11c176201ea4dd2ac66793f3be18907f88308604e09005
     # Compute the SHA512 of the tar.gz file of the REF version via 'openssl sha512 "<downloaded-file-name>.tar.gz"'
     HEAD_REF vcpkg    # Always build from the latest commit of the specified branch if the REF and SHA512 is not set.
-    PATCHES
-        disable-the-example-executable.patch
 )
 
 vcpkg_configure_cmake(
@@ -17,13 +15,14 @@ vcpkg_configure_cmake(
     OPTIONS 
         -DBUILD_TESTING=OFF
         -DBUILD_EXAMPLE=ON
+        -DPKG_INSTALL=ON
 )
 
 # Use CMake to install the package.
 vcpkg_install_cmake() 
 
-# Copy the MathsConfig.cmake
-vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/Maths)
+# Copy the mathsConfig.cmake
+vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/maths)
 
 # Copy the pdb for the debugger. 
 vcpkg_copy_pdbs()
